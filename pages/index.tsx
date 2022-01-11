03d3f9c3281cb275998b18520a21f2fdad7f86b4
Edgarnoriegaontop <edgar.noriega@ontop.com.co>
2022-01-11 10:13:16 -0500

    Nada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,11 @@
 import type { NextPage } from 'next'
+import RecentPosts from '../components/atoms/recentPosts'
 import { Layout } from '../components/organisms/Layout'
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
+      <RecentPosts/>
     </Layout>
   )
 }
