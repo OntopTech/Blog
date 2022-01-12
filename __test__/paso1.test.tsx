be778df0b4c3531f1cf4eb55b13eb3fdf51e6cb7
Cristian Camilo <40009827+3013688230@users.noreply.github.com>
2022-01-12 17:02:47 -0500

    unit test to do

---
+++ __test__/paso1.test.tsx
@@ -0,0 +1,7 @@
+import { Post1 } from "../components/atoms/post1";
+
+describe("Paso 1 route", () => {
+    it("Shouold render a component paso1.tsx", () => {
+        fail("Not implemented");
+    });
+});
