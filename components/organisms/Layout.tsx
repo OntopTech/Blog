fad7e5fceeffb155cc9e1b2bc33f920e609c0eac
SebastianZorrilla <97193783+SebastianZorrilla@users.noreply.github.com>
2022-01-17 10:55:08 -0500

    Update Layout.tsx

--- components/organisms/Layout.tsx
+++ components/organisms/Layout.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import { ReactNode } from 'react'
 import { Categorie } from '../molecules/Categorie'
 import { Footer } from '../molecules/Footer'
-import { NewsLettre } from '../molecules/NewsLettre'
 import { HeaderContainer } from '../molecules/headerContainer'
 import { ContentNewsLettre } from '../molecules/ContentNewsLettre'
 
