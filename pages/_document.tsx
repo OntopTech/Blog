3c1505f9b8a91202619a5efaa0b73761c272462e
Edgarnoriegaontop <edgar.noriega@ontop.com.co>
2022-01-21 10:32:16 -0500

    SEO settings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,18 +42,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-T4EL3NG52J"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-T4EL3NG52J');`,
-            }}
-          />
           <meta charSet="utf-8" />
           <link
             href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap"
